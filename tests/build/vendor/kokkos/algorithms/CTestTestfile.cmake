136451248c0515ff25db87f79d243c4efbddb9a5
matheo.anquetil@cea.fr <anquetilm@ruche-gpu07.cluster>
2022-07-12 16:25:37 +0200

    renamed tests directory to global_variables

--- tests/build/vendor/kokkos/algorithms/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /gpfs/users/anquetilm/kokkos_game_of_life/tests/vendor/kokkos/algorithms
-# Build directory: /gpfs/users/anquetilm/kokkos_game_of_life/tests/build/vendor/kokkos/algorithms
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
--- tests/build/vendor/kokkos/containers/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /gpfs/users/anquetilm/kokkos_game_of_life/tests/vendor/kokkos/containers/src
-# Build directory: /gpfs/users/anquetilm/kokkos_game_of_life/tests/build/vendor/kokkos/containers/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
